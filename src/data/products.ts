571ffc2c33c9a91ed7536a066c01e63f9d8e9384
Konrad <konslo96@gmail.com>
2023-03-28 19:21:11 +0200

    [feat] Add products data

---
+++ src/data/products.ts
@@ -0,0 +1,31 @@
+import spicedMint from "../assets/img/candles/spiced-mint.png";
+import cleanLavander from "../assets/img/candles/clean-lavander.png";
+import juicyLemon from "../assets/img/candles/juicy-lemon.png";
+import sweetStrawberry from "../assets/img/candles/sweet-strawberry.png";
+
+export const products = [
+  {
+    id: 1,
+    name: "Spiced Mint",
+    price: 9.99,
+    image: spicedMint,
+  },
+  {
+    id: 2,
+    name: "Clean Lavander",
+    price: 9.99,
+    image: cleanLavander,
+  },
+  {
+    id: 3,
+    name: "Juicy Lemon",
+    price: 9.99,
+    image: juicyLemon,
+  },
+  {
+    id: 4,
+    name: "Sweet Strawberry",
+    price: 9.99,
+    image: sweetStrawberry,
+  },
+];
