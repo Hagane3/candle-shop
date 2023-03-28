3f5d3640815a544d3acaec83108e43f2ecbf4d18
Konrad <konslo96@gmail.com>
2023-03-28 19:23:19 +0200

    [feat] Add Products section to Homepage file

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
 import classes from "./index.module.scss";
+import Products from "./Products/Products";
 
 const index = () => {
   return (
     <main className={classes.root}>
       <Header />
+      <Products />
     </main>
   );
 };
