d26fe6e77b34cb0e301c9176d84519353bbf5527
Konrad <konslo96@gmail.com>
2023-03-29 17:21:20 +0200

    [feat] Add loadMoreProducts functionality (button)

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -4,11 +4,16 @@ import classes from "./Button.module.scss";
 type Props = {
   children: React.ReactNode;
   width: number;
+  showAllProducts?: () => void;
 };
 
-const Button = ({ children, width }: Props) => {
+const Button = ({ children, width, showAllProducts }: Props) => {
   return (
-    <button className={classes.root} style={{ width: `${width}%` }}>
+    <button
+      className={classes.root}
+      style={{ width: `${width}%` }}
+      onClick={showAllProducts}
+    >
       {children}
     </button>
   );
--- src/pages/Home/Products/Products.tsx
+++ src/pages/Home/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../../components/UI/Button/Button";
 import ProductItem from "../ProductItem/ProductItem";
 
@@ -7,12 +7,18 @@ import { products } from "../../../data/products";
 import classes from "./Products.module.scss";
 
 const Products = () => {
+  const [visibleProducts, setVisibleProducts] = useState(2);
+
+  const showAllProductsHandler = () => {
+    setVisibleProducts(products.length);
+  };
+
   return (
     <section className={classes.root}>
       <h3>Products</h3>
       <h4>Order it for you or for your beloved ones.</h4>
       <div className={classes.products}>
-        {products.map((product) => {
+        {products.slice(0, visibleProducts).map((product) => {
           return (
             <ProductItem
               key={product.id}
@@ -23,7 +29,11 @@ const Products = () => {
           );
         })}
       </div>
-      <Button width={50}>See more</Button>
+      {visibleProducts < products.length && (
+        <Button width={50} showAllProducts={showAllProductsHandler}>
+          See more
+        </Button>
+      )}
     </section>
   );
 };
