b2dc457a20bf1a3fd1666f9c1126e5d0d8d518c6
Konrad <konslo96@gmail.com>
2023-03-29 15:19:04 +0200

    [feat] Add testimonials data

---
+++ src/data/testimonials.ts
@@ -0,0 +1,27 @@
+import user1 from "../assets/img/users/user1.png";
+import user2 from "../assets/img/users/user2.png";
+import user3 from "../assets/img/users/user3.png";
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "Luisa",
+    text: "I love this product. No more air fresheners!",
+    rate: 5,
+    avatar: user1,
+  },
+  {
+    id: 2,
+    name: "Edoardo",
+    text: "Recommended by my wife, she loves it!",
+    rate: 5,
+    avatar: user2,
+  },
+  {
+    id: 3,
+    name: "Mart",
+    text: "Looks very natural, the smell is awesome",
+    rate: 4,
+    avatar: user3,
+  },
+];
