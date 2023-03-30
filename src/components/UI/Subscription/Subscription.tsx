03289c6b20e16089d95064a4017b2c1a3da6af8b
Konrad <konslo96@gmail.com>
2023-03-30 15:56:59 +0200

    [feat] Add Subscription UI component

---
+++ src/components/UI/Subscription/Subscription.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./Subscription.module.scss";
+
+const Subscription = () => {
+  return (
+    <div className={classes.root}>
+      <form>
+        <div className={classes.one_time}>
+          <input type="radio" name="sub" id="onetime" checked />
+          <label htmlFor="onetime">One time purchase</label>
+        </div>
+        <div className={classes.subscription}>
+          <div className={classes.subscription_header}>
+            <input type="radio" name="sub" id="subscription" />
+            <label htmlFor="subscription">
+              <span> Subscribe and delivery every</span>
+              <select>
+                <option value="1 week">1 week</option>
+                <option value="2 weeks">2 weeks</option>
+                <option value="3 weeks">3 weeks</option>
+                <option value="4 weeks">4 weeks</option>
+              </select>
+            </label>
+          </div>
+          <div className={classes.subscription_description}>
+            <p>
+              Subscribe now and get the 10% of discount on every recurring
+              order. The discount will be applied at checkout.{" "}
+              <span>See details</span>
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Subscription;
