7d679ca3b6596420e0198632108116f878dc4833
Konrad <konslo96@gmail.com>
2023-04-01 11:01:41 +0200

    [feat] Add decrease and increase functionalities to QuantityBox component

--- src/components/UI/QuantityBox/QuantityBox.tsx
+++ src/components/UI/QuantityBox/QuantityBox.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import classes from "./QuantityBox.module.scss";
 
 const QuantityBox = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity((prevState) => prevState - 1);
+    }
+  };
+
   return (
     <div className={classes.root}>
       <p>Quantity</p>
       <div className={classes.quantity}>
-        <button>+</button>
-        <span>1</span>
-        <button>-</button>
+        <button onClick={increaseQuantity}>+</button>
+        <span>{quantity}</span>
+        <button onClick={decreaseQuantity}>-</button>
       </div>
     </div>
   );
