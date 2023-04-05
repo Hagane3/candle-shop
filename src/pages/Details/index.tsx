22f64894ebd7d37ccbcd95d96d9d5f2f26a1712d
Konrad <konslo96@gmail.com>
2023-04-05 20:47:01 +0200

    [feat] Add summary product section

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,9 +1,21 @@
+import CartIcon from "../../components/UI/CartIcon/CartIcon";
+import { useSelector } from "react-redux";
 import classes from "./index.module.scss";
 
+import arrowdown from "../../assets/icon/arrowdown.svg";
+
 const index = () => {
+  const { totalAmount } = useSelector((state: any) => state.cart);
+
   return (
     <main className={classes.root}>
-      <div className={classes.summary_product}></div>
+      <div className={classes.summary_product}>
+        <CartIcon />
+        <p>
+          See your order details <img src={arrowdown} />
+        </p>
+        <span className={classes.amount}>{`$${totalAmount}`}</span>
+      </div>
     </main>
   );
 };
