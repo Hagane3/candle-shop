b9f4ccb0ff41e76d80c68bc7aff2f4183f37b357
Konrad <konslo96@gmail.com>
2023-04-07 20:33:50 +0200

    [feat] Add contact/ship to summary info

--- src/pages/Shipping/index.tsx
+++ src/pages/Shipping/index.tsx
@@ -1,13 +1,28 @@
 import { useSelector } from "react-redux";
+
+import classes from "./index.module.scss";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
-import OrderDetailsProduct from "../../components/SummaryCart/OrderDetailsProduct/OrderDetailsProduct";
+import SummaryCart from "../../components/SummaryCart/SummaryCart";
 
 const index = () => {
-  const data = useSelector((state: any) => state);
+  const { order } = useSelector((state: any) => state.order);
+  console.log(order);
 
   return (
-    <main>
+    <main className={classes.root}>
+      <SummaryCart />
       <Breadcrumbs />
+      <div className={classes.summary_info}>
+        <div className={classes.summary_contact}>
+          <p>Contact</p>
+          <span>{`${order.email}`}</span>
+        </div>
+        <span className={classes.line}> </span>
+        <div className={classes.summary_shipping}>
+          <p>Ship to</p>
+          <span>{`${order.address}, ${order.postalCode}, ${order.region}, ${order.country}`}</span>
+        </div>
+      </div>
     </main>
   );
 };
