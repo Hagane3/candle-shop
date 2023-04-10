c28afe96a04a1603c4e4dc7da2c82d4736e41720
Konrad <konslo96@gmail.com>
2023-04-10 15:53:17 +0200

    [feat] Add responsive styling to Benefits section

--- src/pages/Home/Benefits/Benefits.tsx
+++ src/pages/Home/Benefits/Benefits.tsx
@@ -11,45 +11,50 @@ import leaves from "../../../assets/img/leaves.png";
 const Benefits = () => {
   return (
     <section className={classes.root}>
-      <h3>Clean and fragrant soy wax</h3>
-      <h4>Made for your home and for your wellness</h4>
-      <div className={classes.image_container}>
-        <img src={candle1} alt="candle" />
-        <img src={candle2} alt="candle" />
-        <img src={leaves} alt="leaves" />
+      <div className={classes.wrapper}>
+        <div className={classes.heading}>
+          <h3>Clean and fragrant soy wax</h3>
+          <h4>Made for your home and for your wellness</h4>
+        </div>
+        <div className={classes.image_container}>
+          <img src={candle1} alt="candle" />
+          <img src={candle2} alt="candle" />
+          <img src={leaves} alt="leaves" />
+        </div>
+        <div className={classes.benefits}>
+          <ul>
+            <li>
+              <img src={checkmark} alt="checkmark" />{" "}
+              <p>
+                <strong>Eco-sustainable:</strong> All recyclable materials, 0%
+                CO2 emissions
+              </p>
+            </li>
+            <li>
+              <img src={checkmark} alt="checkmark" />
+              <p>
+                <strong>Hyphoallergenic:</strong> 100% natural, human friendly
+                ingredients{" "}
+              </p>
+            </li>
+            <li>
+              <img src={checkmark} alt="checkmark" />
+              <p>
+                <strong>Handmade:</strong> All candles are craftly made with
+                love.
+              </p>
+            </li>
+            <li>
+              <img src={checkmark} alt="checkmark" />
+              <p>
+                <strong>Long burning:</strong> No more waste. Created for last
+                long.
+              </p>
+            </li>
+          </ul>
+          <Button width={50}>Learn more</Button>
+        </div>
       </div>
-      <div className={classes.benefits}>
-        <ul>
-          <li>
-            <img src={checkmark} alt="checkmark" />{" "}
-            <p>
-              <strong>Eco-sustainable:</strong> All recyclable materials, 0% CO2
-              emissions
-            </p>
-          </li>
-          <li>
-            <img src={checkmark} alt="checkmark" />
-            <p>
-              <strong>Hyphoallergenic:</strong> 100% natural, human friendly
-              ingredients{" "}
-            </p>
-          </li>
-          <li>
-            <img src={checkmark} alt="checkmark" />
-            <p>
-              <strong>Handmade:</strong> All candles are craftly made with love.
-            </p>
-          </li>
-          <li>
-            <img src={checkmark} alt="checkmark" />
-            <p>
-              <strong>Long burning:</strong> No more waste. Created for last
-              long.
-            </p>
-          </li>
-        </ul>
-      </div>
-      <Button width={50}>See more</Button>
     </section>
   );
 };
