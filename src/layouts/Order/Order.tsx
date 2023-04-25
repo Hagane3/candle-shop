4ee7a07450ab438bbb88955443e08d230badb93a
Konrad <konslo96@gmail.com>
2023-04-25 19:10:37 +0200

    [fix] Delete ScrollToTop import from Order

--- src/layouts/Order/Order.tsx
+++ src/layouts/Order/Order.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 import OrderNavbar from "./OrderNavbar/OrderNavbar";
-import { ScrollToTop } from "../../utils/ScrollToTop";
 
 const Order = () => {
   return (
-    <ScrollToTop>
+    <>
       <OrderNavbar />
       <Outlet />
-    </ScrollToTop>
+    </>
   );
 };
 
